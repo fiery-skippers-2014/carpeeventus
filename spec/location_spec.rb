de8319d73b14038b121268998e4348a1a0ed8d52
& <pair+darrrelll.jwrobes@devbootcamp.com>
2014-04-18 10:28:31 -0700

    fixed tests; deleted application.js

--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Location do
-  it {should belongs_to :user}
+  it {should belong_to :user}
   it {should validate_uniqueness_of(:latitude)}
   it {should validate_uniqueness_of(:longitude)}
 end
